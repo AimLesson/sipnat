3a5d0b7b33c05085232890e6509e6fd06d4bafad
AimLesson <113356516+AimLesson@users.noreply.github.com>
2024-12-18 22:39:46 +0700

    update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sipnat.aimlesson.online/';
+$config['base_url'] = 'https://semasys.cloud//';
 
 /*
 |--------------------------------------------------------------------------
